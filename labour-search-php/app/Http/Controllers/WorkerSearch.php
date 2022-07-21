361c3fa25aa2dc7b055b62d5fc2981e9d9f4f441
dredgy <72254513+dredgy@users.noreply.github.com>
2022-07-21 14:15:41 +1000

    Laravel Setup

---
+++ labour-search-php/app/Http/Controllers/WorkerSearch.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WorkerSearch extends Controller
+{
+    function run(int $startDate, int $endDate){
+        
+    }
+}
